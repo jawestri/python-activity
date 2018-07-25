f37b1d0ccd033e55896e81029f5dbe70a48b2776
jawestri <33428458+jawestri@users.noreply.github.com>
2018-07-25 16:31:50 -0400

    pylink edit

--- hangman.py
+++ hangman.py
@@ -10,8 +10,8 @@ import random
 # display options below
 
 
-def hangman_visual(num_wrong):
-    if num_wrong == 0:
+def hangman_visual(wrong):
+    if wrong == 0:
         print (" _______")
         print ("    |  |")
         print ("       |")
@@ -19,24 +19,23 @@ def hangman_visual(num_wrong):
         print ("       |")
         print ("       |")
         print (" -------")
-
-    elif num_wrong == 1:
-        print (" _______")
-        print ("    |  |")
-        print ("    O  |")
-        print ("       |")
-        print ("       |")
-        print ("       |")
-        print (" -------")
-    elif num_wrong == 2:
+    elif wrong == 1:
         print (" _______")
         print ("    |  |")
         print ("    O  |")
+        print ("       |")
+        print ("       |")
+        print ("       |")
+        print (" -------")
+    elif wrong == 2:
+        print (" _______")
+        print ("    |  |")
+        print ("    O  |")
         print ("    |  |")
         print ("       |")
         print ("       |")
         print (" -------")
-    elif num_wrong == 3:
+    elif wrong == 3:
         print (" _______")
         print ("    |  |")
         print ("    O  |")
@@ -44,7 +43,7 @@ def hangman_visual(num_wrong):
         print ("       |")
         print ("       |")
         print (" -------")
-    elif num_wrong == 4:
+    elif wrong == 4:
         print (" _______")
         print ("    |  |")
         print ("    O  |")
@@ -52,7 +51,7 @@ def hangman_visual(num_wrong):
         print ("       |")
         print ("       |")
         print (" -------")
-    elif num_wrong == 5:
+    elif wrong == 5:
         print (" _______")
         print ("    |  |")
         print ("    O  |")
@@ -60,7 +59,7 @@ def hangman_visual(num_wrong):
         print ("   /   |")
         print ("       |")
         print (" -------")
-    elif num_wrong == 6:
+    elif wrong == 6:
         print (" _______")
         print ("    |  |")
         print ("    O  |")
@@ -72,51 +71,51 @@ def hangman_visual(num_wrong):
 print("Welcome to Hangman!")
 print("The word is 5 letters and you get 6 wrong choices")
 print("\n\n~~Starting Game~~\n")
-user_guess = []
-word_list = ["phone", "quick", "water", "bravo", "table", "micro",
+USERGUESS = []
+WORDLIST = ["phone", "quick", "water", "bravo", "table", "micro",
              "cable", "giant", "power", "laser", "video"]
-word = random.choice(word_list)
+WORD = random.choice(WORDLIST)
 
-blanks = "_" * 5
-blanks_list = list(blanks)
-print ("Guess the word: {:^5s}".format(' '.join(blanks_list)))
-blanks_left = len(word)
-count = 0
-win = False
-lose = False
-num_wrong = 0
-while win is not True and lose is not True:
-    guess = input('Enter 1 letter:')
+BLANKS = "_" * 5
+BLANKSLIST = list(BLANKS)
+print ("Guess the word: {:^5s}".format(' '.join(BLANKSLIST)))
+BLANKSLEFT = len(WORD)
+COUNT = 0
+WIN = False
+LOSE = False
+NUMWRONG = 0
+while WIN is not True and LOSE is not True:
+    GUESS = input('Enter 1 letter:')
 # if user guesses duplicate letter, display previous attempts
-    if guess in user_guess:
-        print("\nLetter already been guessed!\nYou've tried:%s" % user_guess)
+    if GUESS in USERGUESS:
+        print("\nLetter already been guessed!\nYou've tried:%s" % USERGUESS)
     else:
         # check attempt letter againt each index in word
         # if correct, display loction of correct letter
         # if guess incorrect, inc. num of wrong guesses
-            wrong = True
-            for i in range(0, len(word)):
-                if guess == word[i]:
-                    blanks_list[i] = word[i]
-                    print("correct guess!")
-                    blanks_left -= 1
-                    wrong = False
-            if wrong is True:
-                num_wrong += 1
-                print("letter not in word")
+        WRONG = True
+        for i in range(0, len(WORD)):
+            if GUESS == WORD[i]:
+                BLANKSLIST[i] = WORD[i]
+                print("correct guess!")
+                BLANKSLEFT -= 1
+                WRONG = False
+        if WRONG is True:
+            NUMWRONG += 1
+            print("letter not in word")
 # displaying hangman and progess of user
-    count += 1
-    hangman_visual(num_wrong)
-    print ("{:^30s}".format(' '.join(blanks_list)))
+    COUNT += 1
+    hangman_visual(NUMWRONG)
+    print ("{:^30s}".format(' '.join(BLANKSLIST)))
     print("\n--------------------------------------------------------------\n")
-    user_guess.append(guess)
+    USERGUESS.append(GUESS)
 # all blanks have been filled=user wins
-    if blanks_left == 0:
-        win = True
+    if BLANKSLEFT == 0:
+        WIN = True
         print("Congrats, you won!!!!!")
-    if num_wrong == 6:                        # user has beeen hanged
+    if NUMWRONG == 6:                        # user has beeen hanged
         print("Too many tries, you've been hanged")
-        print("The word was %s" % word)
-        lose = True
+        print("The word was %s" % WORD)
+        LOSE = True
 
         
