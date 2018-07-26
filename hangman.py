fc5e4d65f2612f3e41248230bbc5f3c28e919484
jawestri <33428458+jawestri@users.noreply.github.com>
2018-07-26 13:14:38 -0400

    pep8

--- hangman.py
+++ hangman.py
@@ -67,6 +67,7 @@ def hangman_visual(wrong):
         print("   / \ |")
         print("       |")
         print(" -------")
+        
 
 # setup
 print("Welcome to Hangman!")
@@ -78,7 +79,7 @@ WORDLIST = ["phone", "quick", "water", "bravo", "table", "micro",
 WORD = random.choice(WORDLIST)
 
 BLANKS = "_" * 5
-BLANKSLIST = list(BLANKS)
+BLANKSLIST = list (BLANKS)
 print ("Guess the word: {:^5s}".format(' '.join(BLANKSLIST)))
 BLANKSLEFT = len(WORD)
 COUNT = 0
@@ -107,7 +108,7 @@ while WIN is not True and LOSE is not True:
 
 # displaying hangman and progess of user
     COUNT += 1
-    hangman_visual(NUMWRONG)
+    hangman_visual (NUMWRONG)
     print ("{:^30s}".format(' '.join(BLANKSLIST)))
     print("\n--------------------------------------------------------------\n")
     USERGUESS.append(GUESS)
@@ -120,5 +121,6 @@ while WIN is not True and LOSE is not True:
         print("Too many tries, you've been hanged")
         print("The word was %s" % WORD)
         LOSE = True
+
 
        
